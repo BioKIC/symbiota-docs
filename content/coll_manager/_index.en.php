9264b62b509be49ee55b8f74c717567441bce0cb
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-10-22 08:19:14 -0700

    change collection manager description

--- content/coll_manager/_index.en.php
+++ content/coll_manager/_index.en.php
@@ -3,6 +3,6 @@ title: 'Collection Manager Guide'
 date: 2021-10-22
 icon: 'ti-search'
 weight: 1
-description: 'Learn about advanced data cleaning, permissions management, data upload, and other functions.'
+description: 'Learn to clean data, edit permissions, upload data, and perform other administrator functions.'
 type: 'docs'
 ---
