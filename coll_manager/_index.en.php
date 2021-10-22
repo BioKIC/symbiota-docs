648e764141dc204903fc715af4d609f6c0bca5cf
themerekat <themerekat@users.noreply.github.com>
2021-10-22 15:18:45 +0000

    deploy: ff66468cb369842dca05c5464c6c419b651b2784

---
+++ coll_manager/_index.en.php
@@ -0,0 +1,8 @@
+---
+title: 'Collection Manager Guide'
+date: 2021-10-22
+icon: 'ti-search'
+weight: 1
+description: 'Learn about advanced data cleaning, permissions management, data upload, and other functions.'
+type: 'docs'
+---
